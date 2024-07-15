080b4f65df874ce0c842f528bab0d143a9e54df0
farhanmahmud21 <mahmudkhanfarhan2001@gmail.com>
2024-07-15 16:51:00 +0600

    gesture and inkwell added

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ class Buttons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       home: Scaffold(
-        
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Icon(Icons.add),
@@ -21,31 +19,58 @@ class Buttons extends StatelessWidget {
           title: Text('Button Demo'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                    elevation: 6,
-                    minimumSize: Size(100, 50),
-                    shadowColor: Colors.red,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    side: BorderSide(color: Colors.black, width: 5),
-                    backgroundColor: Colors.amberAccent),
-                onPressed: () {},
-                label: Text('Press Elevated Button'),
-                icon: Icon(Icons.add_a_photo),
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        elevation: 6,
+                        minimumSize: Size(100, 50),
+                        shadowColor: Colors.red,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        side: BorderSide(color: Colors.black, width: 5),
+                        backgroundColor: Colors.amberAccent),
+                    onPressed: () {},
+                    label: Text('Press Elevated Button'),
+                    icon: Icon(Icons.add_a_photo),
+                  ),
+                  TextButton(
+                      onPressed: () {}, child: Text('Press Text Button')),
+                  OutlinedButton(
+                      onPressed: () {}, child: Text('Outlined Button')),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.access_alarm,
+                        size: 60,
+                      )),
+                  GestureDetector(
+                    onTap: () {
+                      print('On Tap Gesture Detector');
+                    },
+                    child: Image.network(
+                      'https://i0.wp.com/flutterexperts.com/wp-content/uploads/2024/04/1_DBbe3BaLTgNr3GlvjiIIgw.png?fit=960%2C553&ssl=1',
+                      height: 170,
+                      width: 190,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print('On Tap Ink Well');
+                    },
+                    child: Image.network(
+                      'https://www.scaler.com/topics/images/inkwell-flutter_thumbnail.webp',
+                      height: 120,
+                      width: 200,
+                    ),
+                  )
+                ],
               ),
-              TextButton(onPressed: () {}, child: Text('Press Text Button')),
-              OutlinedButton(onPressed: () {}, child: Text('Outlined Button')),
-              IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.access_alarm,
-                    size: 60,
-                  ))
-            ],
+            ),
           ),
         ),
       ),
